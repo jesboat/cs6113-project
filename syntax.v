7af6c8f4dc5b3246885162d19ed21cf6dd86f13c
Matthew Milano <milano@cs.cornell.edu>
2013-12-14 23:41:20 -0500

    refined syntax

--- syntax.v
+++ syntax.v
@@ -1,14 +1,35 @@
-Module CoreMLSynatx.
-  
+Module CoreMLSyntax.
+
+
+(*racket-dependent *)
   Inductive variable_name : Type := 
-  | sample_variable_name : variable_name.
+  | x : variable_name
+  | y : variable_name.
+
+(*non-racket-dependent *)
+
+  Inductive high_label : Type := 
+  | High_Label_t : high_label.
   
+  Inductive label : Type := 
+  | High_Label : high_label -> label
+  | Low_Label : label.
+
+  Inductive simple_type : Type :=
+  | Int_t : simple_type
+  | Fix_t : simple_type -> simple_type -> simple_type
+  | Bottom_t : simple_type.
+  
+  Inductive type : Type :=
+  | Type_type : simple_type -> label -> type.
+
+
+
   Inductive value : Type :=
   | Identifier : variable_name -> value
   | Unit :  value
   | Integer : nat -> value
   | Fix : variable_name -> expression -> value 
-  | Tuple : value -> value -> value
   | Void : value
   | Value_Evaluation_Pair : value -> value -> value
 
@@ -17,10 +38,12 @@ Module CoreMLSynatx.
   | Value : value -> expression 
   | Application : value -> value -> expression
   | Let_Bind : variable_name -> value -> expression -> expression
-  | If : expression -> expression -> expression -> expression
-  |	Expression_Evaluation_Pair : expression -> expression -> expression
-  | Project1 : value -> expression
-  | Project2 : value -> expression.
+  | If1 : expression -> expression -> expression -> expression
+  | Expression_Evaluation_Pair : expression -> expression -> expression.
 
-	
-End CoreMLSynatx.
+Check Let_Bind (x) 
+      ( Integer 3) (Value ( Fix (y) (Value ( Identifier x)) )) .
+
+End CoreMLSyntax.
+
+
