0fdff45ed43c349432555b021f634f5f577f1aeb
Matthew Milano <milano@cs.cornell.edu>
2013-12-14 19:33:30 -0500

    new syntax regime

--- syntax.v
+++ syntax.v
@@ -11,38 +11,38 @@ Module CoreMLSyntax.
   Inductive high_label : Type := 
   | High_Label_t : high_label.
   
-  Inductive label : Type := 
-  | High_Label : high_label -> label
-  | Low_Label : label.
+  Inductive label_type : Type := 
+  | High_Label : high_label -> label_type
+  | Low_Label : label_type
+  | Unassigned_Label : label_type.
+(* proof of correctness for labels will be no occurance of "unassigned" label
+   in addition to the standard types-line-up deal.
+*)
 
   Inductive simple_type : Type :=
   | Int_t : simple_type
-  | Fix_t : simple_type -> simple_type -> simple_type
   | Bottom_t : simple_type.
-  
+
   Inductive type : Type :=
-  | Type_type : simple_type -> label -> type.
-
-
-
+  | Simple_Type : simple_type -> label_type -> type
+  | Fix_t : type -> type -> label_type  -> type.
+  
   Inductive value : Type :=
-  | Identifier : variable_name -> value
-  | Unit :  value
-  | Integer : nat -> value
-  | Fix : variable_name -> expression -> value 
-  | Void : value
-  | Value_Evaluation_Pair : value -> value -> value
+  | Identifier : type -> variable_name -> value
+  | Unit :  type -> value
+  | Integer : type -> nat -> value
+  | Fix : type -> variable_name -> expression -> value 
+  | Void : type -> value
+  | Value_Evaluation_Pair : type -> value -> value -> value
 
   with 
   expression : Type :=
-  | Value : value -> expression 
-  | Application : value -> value -> expression
-  | Let_Bind : variable_name -> value -> expression -> expression
-  | If1 : expression -> expression -> expression -> expression
-  | Expression_Evaluation_Pair : expression -> expression -> expression.
+  | Value : type -> value -> expression 
+  | Application : type -> value -> value -> expression
+  | Let_Bind : type -> variable_name -> value -> expression -> expression
+  | If1 : type -> value -> expression -> expression -> expression
+  | Expression_Evaluation_Pair : type -> expression -> expression -> expression.
 
-Check Let_Bind (x) 
-      ( Integer 3) (Value ( Fix (y) (Value ( Identifier x)) )) .
 
 End CoreMLSyntax.
 
